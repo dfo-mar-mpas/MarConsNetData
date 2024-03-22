8e2d52c3ec7799774277a866e3186e16f74188ee
Remi Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2024-03-22 14:11:33 -0300

    add get_bioregion()

---
+++ R/get_bioregion.R
@@ -0,0 +1,13 @@
+#' get_bioregion
+#'
+#' @param bioregion name of the bioregion to match from the `NAME_E` column from [Open Data](https://gisp.dfo-mpo.gc.ca/arcgis/rest/services/FGP/Federal_Marine_Bioregions/MapServer/0). Default is `"Scotian Shelf"`
+#'
+#' @return sf data.frame
+#' @export
+#'
+#' @examples
+#' bioregion <- get_bioregion()
+get_bioregion <- function(bioregion = "Scotian Shelf"){
+  bioregion <- arcpullr::get_spatial_layer("https://gisp.dfo-mpo.gc.ca/arcgis/rest/services/FGP/Federal_Marine_Bioregions/MapServer/0",
+                                           where=paste0("NAME_E='",bioregion,"'"))
+}
