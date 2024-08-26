fd4e6f05fc39b6c76d353becd8a987e220d11d7d
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2024-08-26 14:09:35 -0300

    update ctd

--- R/project_AZMP.R
+++ R/project_AZMP.R
@@ -9,6 +9,9 @@
 #' CTD data from the spring program is returned. If summer or winter
 #' is given, the CTD ground fish data is given from the respective
 #' season.
+#' @param destdir a directory indicating where to save the fetched data.
+#' @param force A boolean indicating if a new version of this data should
+#' be obtained or not
 #'
 #' @importFrom XML read_html
 #' @importFrom rvest html_attr
@@ -19,19 +22,25 @@
 #' @return A list of NetCDF files downloaded in your destdir
 #' @export
 
-project_AZMP <- function(season="fall", destdir="~/data/AZMP/") {
+project_AZMP <- function(season="fall", destdir="tmp", force=FALSE) {
   if (is.null(season)) {
     stop("Must provide a type argument of either fall, spring, summer, fall, or all")
   }
 
-  if (!(season %in% c("fall", "spring", "summer", "winter", "all"))) {
+  if (!(all(season %in% c("fall", "spring", "summer", "winter", "all")))) {
     stop("Must provide a type argument of either fall, spring, summer, winter or all")
   }
 
-  if (season == "all") {
+  if (all(season== "all")) {
     TYPE <- c("fall", "summer", "spring", "winter")
   } else {
     TYPE <- season
+  }
+
+
+  if (!(file.exists("~/data/AZMP/CTDdf.rda")) | force) {
+  if (destdir == "tmp") {
+    destdir <- tempfile()
   }
   CTD <- vector("list", length(TYPE))
   names(CTD) <- TYPE
@@ -74,7 +83,6 @@ project_AZMP <- function(season="fall", destdir="~/data/AZMP/") {
 
   CTD[[i]] <- vector("list", length(years))
   names(CTD[[i]]) <- years
-
   for (y in seq_along(years)) {
     # Create a folder with the directory name in the destination directory
     y_path <- file.path(destdir, years[[y]])
@@ -100,13 +108,19 @@ project_AZMP <- function(season="fall", destdir="~/data/AZMP/") {
 
     # Download each ODF file
     for (file in seq_along(odf_files)) {
+      message("file = ", file)
       file_url <- paste0(y_url, odf_files[[file]])
       file_dest <- file.path(y_path, odf_files[[file]])
 
       # Download the file
       GET(file_url, write_disk(file_dest, overwrite = TRUE))
-      nc <- nc_open(file_dest)
-      pressure <- ncvar_get(nc, "PRESPR01")
+      nc <- try(nc_open(file_dest), silent=TRUE)
+      if (!(inherits(nc, "try-error"))) {
+        if ("PRESPR01" %in% names(nc$var)) {
+          pressure <- ncvar_get(nc, "PRESPR01")
+        } else {
+          pressure <- 0
+        }
       if ("TEMPPR01" %in% names(nc$var)) {
         temperature <- ncvar_get(nc, "TEMPPR01")
       } else {
@@ -116,14 +130,42 @@ project_AZMP <- function(season="fall", destdir="~/data/AZMP/") {
       longitude <- ncvar_get(nc, "longitude")
       latitude <- ncvar_get(nc, "latitude")
       time <- numberAsPOSIXct(ncvar_get(nc, "time"))
+      if (!(length(unique(pressure)) == 1)) {
       ctd <- as.ctd(pressure=pressure, temperature=temperature, salinity=salinity,
                     longitude=longitude, latitude=latitude, time=time)
       ctd@metadata$filename <- odf_files[file]
+      } else {
+        CTD[[i]][[y]][file] <- 0
+      }
 
       CTD[[i]][[y]][file] <- ctd
+      } else {
+        CTD[[i]][[y]][file] <- 0
+      }
     }
   }
-  return(CTD)
 
 } # End of all
+  #browser()
+
+  save(CTD, file=paste0("~/data/AZMP/CTD.rda"))
+  longitude <- CTD
+  latitude <- CTD
+  for (a in seq_along(CTD)) {
+    for (j in seq_along(CTD[[a]])) {
+      for (k in seq_along(CTD[[a]][[j]])) {
+        longitude[[a]][[j]][k] <- CTD[[a]][[j]][k][[1]][['longitude']]
+        latitude[[a]][[j]][k] <- CTD[[a]][[j]][k][[1]][['latitude']]
+      }
+    }
+  }
+
+  df <- data.frame(latitude=unname(unlist(latitude)), longitude=unname(unlist(longitude)))
+  save(df, file=paste0("~/data/AZMP/CTDdf.rda"))
+
+  } else {
+    load(paste0("~/data/AZMP/CTDdf.rda"))
+  }
+  return(df)
+
 }
