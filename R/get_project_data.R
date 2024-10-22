804a5e53513be72b09e6f3f9db20d7dfaed6d228
Remi Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2024-10-22 12:37:25 -0300

    Update get_project_data.R

--- R/get_project_data.R
+++ R/get_project_data.R
@@ -78,7 +78,7 @@ get_project_data <- function(ids=NULL, titles=NULL, taxize=FALSE, MPA=NULL) {
     df <- project_optimizingeDNA(taxize=taxize)
   } else if (id == "579" | title == "AZMP") {
     #browser("This is a problem") # FIXME
-    df <- project_AZMP(taxize=taxize)
+    df <- project_AZMP()
   }
     LIST[[II]] <- df
     }
