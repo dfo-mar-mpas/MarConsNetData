70b07c7f79338b47f93e01b1bfa392deef01659e
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2024-10-22 08:29:57 -0300

    remove azmp project

--- create_data/data.R
+++ create_data/data.R
@@ -1,10 +1,10 @@
-id <- c(1093, 642, 1491, 579, 428)
+id <- c(1093, 642, 1491, 428)
 df <- data.frame(matrix(ncol = length(c("id", "title", "get_function", "package", "source")), nrow = length(id)))
 colnames(df) <- c("id", "title", "get_function","package", "source")
-df$title <- c("snowCrabSurvey", "eDNAMetabarcoding", "optimizingeDNA", "AZMP", "argoProgram")
-df$get_function <- c("project_snowCrabSurvey", "project_eDNAMetabarcoding", "project_optimizingeDNA", "project_AZMP", "getIndex")
-df$package <- c("MarConsNetData", "MarConsNetData","MarConsNetData","MarConsNetData", "argoFloats")
-df$source <- c("internal", "internal", "internal","internal", "internal")
+df$title <- c("snowCrabSurvey", "eDNAMetabarcoding", "optimizingeDNA", "argoProgram")
+df$get_function <- c("project_snowCrabSurvey", "project_eDNAMetabarcoding", "project_optimizingeDNA", "getIndex")
+df$package <- c("MarConsNetData", "MarConsNetData","MarConsNetData","argoFloats")
+df$source <- c("internal", "internal", "internal","internal")
 df$id <- id
 dataTable <- df
 save(dataTable, file="./data/dataTable.rda")
