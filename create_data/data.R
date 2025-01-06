cd826664785693041411634343e04ee7acd029d4
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2025-01-06 09:11:48 -0400

    Target dataTable

--- create_data/data.R
+++ create_data/data.R
@@ -2,7 +2,7 @@ id <- c(1093, 642, 1491, 428, 579, 726)
 df <- data.frame(matrix(ncol = length(c("id", "title", "get_function", "package", "source")), nrow = length(id)))
 colnames(df) <- c("id", "title", "get_function","package", "source")
 df$title <- c("snowCrabSurvey", "eDNAMetabarcoding", "optimizingeDNA", "argoProgram", "AZMP", "rvSurvey")
-df$get_function <- c("project_snowCrabSurvey", "project_eDNAMetabarcoding", "project_optimizingeDNA", "getIndex", "project_AZMP", "tar_load('gsdet'); tar_load('all_haddock')")
+df$get_function <- c("project_snowCrabSurvey", "project_eDNAMetabarcoding", "project_optimizingeDNA", "getIndex", "project_AZMP", "TARGET::rv_rounded_location")
 df$package <- c("MarConsNetData", "MarConsNetData","MarConsNetData","argoFloats", "MarConsNetData", "Mar.datawrangling")
 df$source <- c("internal", "internal", "internal","internal", "internal", "internal")
 df$id <- id
