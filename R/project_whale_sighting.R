7d9a1577e5e7a741a4c19461acd52415eba9fc04
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2025-01-16 13:48:12 -0400

    whale sighting

---
+++ R/project_whale_sighting.R
@@ -0,0 +1,17 @@
+#' Obtain Whale Sighting data
+#'
+#' @return a data frame containing number of whale sightings per year for WEBCA
+#' @export
+#'
+#' @examples
+project_whale_sighting <- function() {
+
+files <- list.files(file.path(Sys.getenv("OneDriveCommercial"),"MarConsNetTargets","data", "whale"), full.names = TRUE)
+
+f2 <- read.csv(files[2])
+year <- strsplit(f2$ws_date, "-")
+year <- unlist(lapply(year, function(x) x[1]))
+f2$year <- year
+
+return(f2)
+}
--- create_data/data.R
+++ create_data/data.R
@@ -1,10 +1,10 @@
-id <- c(1093, 642, 1491, 428, 579, 726)
+id <- c(1093, 642, 1491, 428, 579, 726, 1062)
 df <- data.frame(matrix(ncol = length(c("id", "title", "get_function", "package", "source")), nrow = length(id)))
 colnames(df) <- c("id", "title", "get_function","package", "source")
-df$title <- c("snowCrabSurvey", "eDNAMetabarcoding", "optimizingeDNA", "argoProgram", "AZMP", "rvSurvey")
-df$get_function <- c("project_snowCrabSurvey", "project_eDNAMetabarcoding", "project_optimizingeDNA", "getIndex", "project_AZMP", "TARGET::rv_rounded_location")
-df$package <- c("MarConsNetData", "MarConsNetData","MarConsNetData","argoFloats", "MarConsNetData", "Mar.datawrangling")
-df$source <- c("internal", "internal", "internal","internal", "internal", "internal")
+df$title <- c("snowCrabSurvey", "eDNAMetabarcoding", "optimizingeDNA", "argoProgram", "AZMP", "rvSurvey", "whaleSightings")
+df$get_function <- c("project_snowCrabSurvey", "project_eDNAMetabarcoding", "project_optimizingeDNA", "getIndex", "project_AZMP", "TARGET::rv_rounded_location", "project_whale_sighting")
+df$package <- c("MarConsNetData", "MarConsNetData","MarConsNetData","argoFloats", "MarConsNetData", "Mar.datawrangling", "MarConsNetData")
+df$source <- c("internal", "internal", "internal","internal", "internal", "internal", "internal")
 df$id <- id
 dataTable <- df
 save(dataTable, file="./data/dataTable.rda")
