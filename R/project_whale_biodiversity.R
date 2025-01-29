467d9acedfe1ce0184f5cf9a711fc929fed05269
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2025-01-29 10:57:22 -0400

    fix whale df

--- R/project_whale_biodiversity.R
+++ R/project_whale_biodiversity.R
@@ -9,8 +9,11 @@ files <- list.files(file.path(Sys.getenv("OneDriveCommercial"),"MarConsNetTarget
 f2 <- read.csv(files[2])
 year <- strsplit(f2$ws_date, "-")
 year <- unlist(lapply(year, function(x) x[1]))
-f2$year <- year
+f2$year <- as.numeric(year)
 f2$type <- "Whale sighting"
+f2$whale_biodiversity <- f2$species_name
 
-return(f2)
+df <- f2[,c("latitude", "longitude", "whale_biodiversity", "year", "type")]
+df$units <- "# of unique species"
+return(df)
 }
